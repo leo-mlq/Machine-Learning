97a3b4d7540cc4a29d81056e65009e58e4764c59
leo-mlq <46854501+leo-mlq@users.noreply.github.com>
2020-05-12 15:32:39 -0700

    Wrapped get average and index into a function
    cleaned up

--- mp2_starter_package/buildTree.py
+++ mp2_starter_package/buildTree.py
@@ -7,50 +7,9 @@ data_set = json.load(open('train.json'))
 train_data = data_set['data']
 train_label = data_set['label']
 
-
-#print(len(train_data[0]));
-#----------------------------------------------
-##each feature array is 19 features, 
-# 0)(integer) The binary result of quality assessment. 0 = bad quality 1 = sufficient quality.
-# 1)(integer) The binary result of pre-screening, where 1 indicates severe retinal abnormality and 0 its lack.
-# 2-7)(integer) The results of MA detection. Each feature value stand for the
-# number of MAs found at the confidence levels alpha = 0.5, . . . , 1, respectively.
-# 8-15)(continuous) contain the same information as 2-7) for exudates. However,
-# as exudates are represented by a set of points rather than the number of
-# pixels constructing the lesions, these features are normalized by dividing the
-# number of lesions with the diameter of the ROI to compensate different image
-# sizes.
-# 16)(continuous) The euclidean distance of the center of
-# the macula and the center of the optic disc to provide important information
-# regarding the patient's condition. This feature
-# is also normalized with the diameter of the ROI.
-# 17)(continuous) The diameter of the optic disc.
-# 18)(integer) The binary result of the AM/FM-based classification.
-
-
-#print(train_data[122])
-#-----------------------------------------------
-##feature contains both integers and continuous values: [1.0, 1.0, 19.0, 15.0, 14.0, 13.0, 12.0, 9.0, 110.744848, 61.295658, 13.508504, 1.534707, 0.073311, 0.005788, 0.001929, 0.000965, 0.474601, 0.090674, 1.0]
-
-#print(len(train_data));
-#-----------------------------------------------
-##a total of 920 feature sets 
-
-#print(train_label);
-#-----------------------------------------------
-##result is either 0 or 1
-
 train_data_np = np.array(train_data)
 train_label_np = np.array([train_label])
-#print(train_data_np.shape)
-##920 X 19
-#print(train_label_np.shape)
-##1 X 920
-
-
-#append label to the end of feature array
 data_set_np = np.concatenate((train_data_np,train_label_np.T),axis=1)
-#print(data_set_np[122])
 
 def col_sparse(dataset, col_inds, attach_labels=True):
 	dataset_new = []
@@ -76,12 +35,29 @@ def cal_entropy(labels):
 		cnt += -label_set[key]/total_labels*np.log2(label_set[key]/total_labels)
 
 	return cnt
-#print(cal_entropy(data_set_np[:,-1]))
-##entropy of whole dataset
+
+def get_averageAndindex(dataset):
+	total=0
+	for i in range(dataset.shape[0]):
+			total+=(float(dataset[i][0]))
+	average = total/dataset.shape[0]
+
+	dneg_row_inds=[]
+	dpos_row_inds=[]
+	for l in range(dataset.shape[0]):
+		
+		if(float(dataset[:,0][l])<average):
+			dneg_row_inds.append(l)
+		elif(float(dataset[:,0][l])>=average):
+			dpos_row_inds.append(l)
+	
+	return average, dneg_row_inds, dpos_row_inds
+	
 
 def cal_gain_ratio(dataset, feature_set_ind, entropy):
 
 	if(feature_set_ind<=7 or feature_set_ind==18):
+		print(feature_set_ind)
 
 		#passed in is col sparsed, first col is feature set, second col is label
 		uniques = np.unique(dataset[:,0])
@@ -98,76 +74,58 @@ def cal_gain_ratio(dataset, feature_set_ind, entropy):
 		gain_ratio = (entropy-feature_set_entropy)/iv
 		return gain_ratio
 	else:
+		print(feature_set_ind)
+
 		#sort by the fist col, feature set
-		dataset_sort=dataset[np.argsort(dataset[:, 0])]
-		#print(dataset_sort[:,0])
+		#dataset_sort=dataset[np.argsort(dataset[:, 0])]
+
 		samples_nums = dataset.shape[0]
-		dataset_mid = []
 		max_ratio=0
-		total=0
+		
 		# for i in range(dataset.shape[0]-1):
 		# 	dataset_mid.append((float(dataset_sort[i][0])+float(dataset_sort[i+1][0]))/2)
-		for i in range(dataset.shape[0]):
-			total+=(float(dataset[i][0]))
+		# dataset_mid.append(total/dataset.shape[0])
+
 
 		##divide continuous value upon average to make it discrete
-		dataset_mid.append(total/dataset.shape[0])
-		# print(sum)
+		average,dneg_row_inds,dpos_row_inds = get_averageAndindex(dataset)
 
-		#print(dataset_mid)
+		dneg_subset=row_sparse(dataset,dneg_row_inds)
+		dpos_subset=row_sparse(dataset,dpos_row_inds)
 
-		for mid in dataset_mid:
-			dneg_row_inds=[]
-			dpos_row_inds=[]
-			for l in range(dataset.shape[0]):
-				
-				if(float(dataset_sort[:,0][l])<mid):
-					dneg_row_inds.append(l)
-				elif(float(dataset_sort[:,0][l])>=mid):
-					dpos_row_inds.append(l)
+		dneg_ent, dpos_ent=0,0;
+		dneg_iv, dpos_iv=0,0;
 
-			dneg_subset=row_sparse(dataset_sort,dneg_row_inds)
-			dpos_subset=row_sparse(dataset_sort,dpos_row_inds)
-			
-			dneg_ent, dpos_ent=0,0;
-			dneg_iv, dpos_iv=0,0;
 
-			# print('neg')
-			# print('--------------------')
-			if(dneg_subset.shape[0]!=0):
-				# print(dneg_subset)
-				dneg_uniques = np.unique(dneg_subset[:,0])
-				for u in dneg_uniques:
-					#passed in is col sparsed, first col is feature set, second col is label
-					row_inds = list(np.where(dneg_subset[:,0]==u)[0])
-					sub_dataset = row_sparse(dneg_subset,row_inds)
-					dneg_ent+=sub_dataset.shape[0]/dneg_subset.shape[0]*cal_entropy(sub_dataset[:,-1])
-					dneg_iv-=sub_dataset.shape[0]/dneg_subset.shape[0]*np.log2(sub_dataset.shape[0]/dneg_subset.shape[0])
-			# print(dneg_ent)
-			# print('pos')
-			# print('--------------------')
-			if(dpos_subset.shape[0]!=0):
-				# print(dpos_subset)
-				dpos_uniques = np.unique(dpos_subset[:,0])
-				for u in dpos_uniques:
-					#passed in is col sparsed, first col is feature set, second col is label
-					row_inds = list(np.where(dpos_subset[:,0]==u)[0])
-					sub_dataset = row_sparse(dpos_subset,row_inds)
-					dpos_ent+=sub_dataset.shape[0]/dpos_subset.shape[0]*cal_entropy(sub_dataset[:,-1])
-					dpos_iv-=sub_dataset.shape[0]/dpos_subset.shape[0]*np.log2(sub_dataset.shape[0]/dpos_subset.shape[0])
-			
-			#print(dpos_ent)
-			gain=entropy-(dneg_subset.shape[0]/samples_nums*dneg_ent+dpos_subset.shape[0]/samples_nums*dpos_ent)
-			# iv=-dneg_subset.shape[0]/samples_nums*np.log2(dneg_subset.shape[0]/samples_nums)-dpos_subset.shape[0]/samples_nums*np.log2(dpos_subset.shape[0]/samples_nums)
-			iv = (dneg_subset.shape[0]/samples_nums)*dneg_iv+(dpos_subset.shape[0]/samples_nums)*dpos_iv
-			# print(dneg_iv)
-			# print(dpos_iv)
-			# print(iv)
-			gain_ratio=gain/iv
-			if(gain_ratio>max_ratio):
-				max_ratio=gain_ratio
-		# print('-----------------')
-		return (max_ratio)
+		if(dneg_subset.shape[0]!=0):
+
+			dneg_uniques = np.unique(dneg_subset[:,0])
+			for u in dneg_uniques:
+				#passed in is col sparsed, first col is feature set, second col is label
+				row_inds = list(np.where(dneg_subset[:,0]==u)[0])
+				sub_dataset = row_sparse(dneg_subset,row_inds)
+				dneg_ent+=sub_dataset.shape[0]/dneg_subset.shape[0]*cal_entropy(sub_dataset[:,-1])
+				dneg_iv-=sub_dataset.shape[0]/dneg_subset.shape[0]*np.log2(sub_dataset.shape[0]/dneg_subset.shape[0])
+
+		if(dpos_subset.shape[0]!=0):
+
+			dpos_uniques = np.unique(dpos_subset[:,0])
+			for u in dpos_uniques:
+				#passed in is col sparsed, first col is feature set, second col is label
+				row_inds = list(np.where(dpos_subset[:,0]==u)[0])
+				sub_dataset = row_sparse(dpos_subset,row_inds)
+				dpos_ent+=sub_dataset.shape[0]/dpos_subset.shape[0]*cal_entropy(sub_dataset[:,-1])
+				dpos_iv-=sub_dataset.shape[0]/dpos_subset.shape[0]*np.log2(sub_dataset.shape[0]/dpos_subset.shape[0])
+		
+
+		gain=entropy-(dneg_subset.shape[0]/samples_nums*dneg_ent+dpos_subset.shape[0]/samples_nums*dpos_ent)
+		# iv=-dneg_subset.shape[0]/samples_nums*np.log2(dneg_subset.shape[0]/samples_nums)-dpos_subset.shape[0]/samples_nums*np.log2(dpos_subset.shape[0]/samples_nums)
+		iv = (dneg_subset.shape[0]/samples_nums)*dneg_iv+(dpos_subset.shape[0]/samples_nums)*dpos_iv
+		gain_ratio=gain/iv
+		if(gain_ratio>max_ratio):
+			max_ratio=gain_ratio
+
+	return (max_ratio)
 
 
 
@@ -208,7 +166,7 @@ def majorLabel(labels):
 
 def TreeGenerate(dataset, features):
 	# data=col_sparse(dataset,features, attach_labels=False)
-	print(dataset)
+
 	labels=dataset[:,-1]
 	labels_uniqes=np.unique(labels)
 	
@@ -220,18 +178,16 @@ def TreeGenerate(dataset, features):
 	if(len(features))==0 or areRowsIdentical(col_sparse(dataset,features, attach_labels=False)):
 		return majorLabel(labels)
 	
-	# print(features)
+
 	best_feature_ind = cal_best_feature(dataset,features)
 	best_feature = features[best_feature_ind]
-	print(best_feature_ind)
-	print(best_feature)
 
 	data = dataset[:,best_feature_ind]
-	print(data)
+
 	dTree={best_feature:{}}
 	
 	data_uniques = np.unique(data)
-	print(data_uniques)
+
 	for u in data_uniques:
 		subfeatures=features.copy()
 		row_inds = list(np.where(data==u))[0]
@@ -240,24 +196,21 @@ def TreeGenerate(dataset, features):
 		else:
 			subdataset=row_sparse(dataset,row_inds)
 			subfeatures.pop(best_feature_ind)
-			print(features)
+			# print(features)
 			dTree[best_feature][u]=TreeGenerate(subdataset,subfeatures)
 	return dTree
+
+		
 		
 
-data_set_np = np.array([[0, 0, 0, 0, 'N'],
-           [0, 0, 0, 1, 'N'],
-           [1, 0, 0, 0, 'Y'],
-           [2, 1, 0, 0, 'Y'],
-           [2, 2, 1, 0, 'Y'],
-           [2, 2, 1, 1, 'N'],
-           [1, 2, 1, 1, 'Y']])
-features_inds=[0,1,2,3]
+# data_set_np = np.array([[0, 0, 0, 0, 'N'],
+#            [0, 0, 0, 1, 'N'],
+#            [1, 0, 0, 0, 'Y'],
+#            [2, 1, 0, 0, 'Y'],
+#            [2, 2, 1, 0, 'Y'],
+#            [2, 2, 1, 1, 'N'],
+#            [1, 2, 1, 1, 'Y']])
+# features=[0,1,2,3]
+features = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
 
-
-#print(TreeGenerate(data_set_np, features_inds))
-
-
-#features_inds = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
-#cal_best_feature(data_set_np,features)
-
+TreeGenerate(data_set_np,features)
