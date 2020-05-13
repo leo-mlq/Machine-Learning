a216d5147a9884cd17de65f1704403c7dd6c92c0
leo-mlq <46854501+leo-mlq@users.noreply.github.com>
2020-05-12 21:29:53 -0700

    Built Tree
    Prediction needed

--- mp2_starter_package/buildTree.py
+++ mp2_starter_package/buildTree.py
@@ -7,8 +7,49 @@ data_set = json.load(open('train.json'))
 train_data = data_set['data']
 train_label = data_set['label']
 
+#print(len(train_data[0]));	
+#----------------------------------------------	
+##each feature array is 19 features, 	
+# 0)(integer) The binary result of quality assessment. 0 = bad quality 1 = sufficient quality.	
+# 1)(integer) The binary result of pre-screening, where 1 indicates severe retinal abnormality and 0 its lack.	
+# 2-7)(integer) The results of MA detection. Each feature value stand for the	
+# number of MAs found at the confidence levels alpha = 0.5, . . . , 1, respectively.	
+# 8-15)(continuous) contain the same information as 2-7) for exudates. However,	
+# as exudates are represented by a set of points rather than the number of	
+# pixels constructing the lesions, these features are normalized by dividing the	
+# number of lesions with the diameter of the ROI to compensate different image	
+# sizes.	
+# 16)(continuous) The euclidean distance of the center of	
+# the macula and the center of the optic disc to provide important information	
+# regarding the patient's condition. This feature	
+# is also normalized with the diameter of the ROI.	
+# 17)(continuous) The diameter of the optic disc.	
+# 18)(integer) The binary result of the AM/FM-based classification.	
+
+
+#print(train_data[122])	
+#-----------------------------------------------	
+##feature contains both integers and continuous values: [1.0, 1.0, 19.0, 15.0, 14.0, 13.0, 12.0, 9.0, 110.744848, 61.295658, 13.508504, 1.534707, 0.073311, 0.005788, 0.001929, 0.000965, 0.474601, 0.090674, 1.0]	
+
+#print(len(train_data));	
+#-----------------------------------------------	
+##a total of 920 feature sets 	
+
+#print(train_label);	
+#-----------------------------------------------	
+##result is either 0 or 1	
+
+
 train_data_np = np.array(train_data)
 train_label_np = np.array([train_label])
+
+#print(train_data_np.shape)	
+##920 X 19	
+#print(train_label_np.shape)	
+##1 X 920	
+
+
+#append label to the end of feature array
 data_set_np = np.concatenate((train_data_np,train_label_np.T),axis=1)
 
 def col_sparse(dataset, col_inds, attach_labels=True):
@@ -37,18 +78,19 @@ def cal_entropy(labels):
 	return cnt
 
 def get_averageAndindex(dataset):
+
 	total=0
-	for i in range(dataset.shape[0]):
-			total+=(float(dataset[i][0]))
-	average = total/dataset.shape[0]
+	for i in range(len(dataset)):
+			total+=(float(dataset[i]))
+	average = total/len(dataset)
 
 	dneg_row_inds=[]
 	dpos_row_inds=[]
-	for l in range(dataset.shape[0]):
+	for l in range(len(dataset)):
 		
-		if(float(dataset[:,0][l])<average):
+		if(float(dataset[l])<average):
 			dneg_row_inds.append(l)
-		elif(float(dataset[:,0][l])>=average):
+		elif(float(dataset[l])>=average):
 			dpos_row_inds.append(l)
 	
 	return average, dneg_row_inds, dpos_row_inds
@@ -57,7 +99,7 @@ def get_averageAndindex(dataset):
 def cal_gain_ratio(dataset, feature_set_ind, entropy):
 
 	if(feature_set_ind<=7 or feature_set_ind==18):
-		print(feature_set_ind)
+
 
 		#passed in is col sparsed, first col is feature set, second col is label
 		uniques = np.unique(dataset[:,0])
@@ -74,8 +116,6 @@ def cal_gain_ratio(dataset, feature_set_ind, entropy):
 		gain_ratio = (entropy-feature_set_entropy)/iv
 		return gain_ratio
 	else:
-		print(feature_set_ind)
-
 		#sort by the fist col, feature set
 		#dataset_sort=dataset[np.argsort(dataset[:, 0])]
 
@@ -88,10 +128,12 @@ def cal_gain_ratio(dataset, feature_set_ind, entropy):
 
 
 		##divide continuous value upon average to make it discrete
-		average,dneg_row_inds,dpos_row_inds = get_averageAndindex(dataset)
+		#passed in is col sparsed, first col is feature set, second col is label
+		average,dneg_row_inds,dpos_row_inds = get_averageAndindex(dataset[:,0])
 
 		dneg_subset=row_sparse(dataset,dneg_row_inds)
 		dpos_subset=row_sparse(dataset,dpos_row_inds)
+
 
 		dneg_ent, dpos_ent=0,0;
 		dneg_iv, dpos_iv=0,0;
@@ -164,9 +206,8 @@ def majorLabel(labels):
 
 
 
-def TreeGenerate(dataset, features):
+def TreeGenerate(dataset, features, depth):
 	# data=col_sparse(dataset,features, attach_labels=False)
-
 	labels=dataset[:,-1]
 	labels_uniqes=np.unique(labels)
 	
@@ -182,22 +223,45 @@ def TreeGenerate(dataset, features):
 	best_feature_ind = cal_best_feature(dataset,features)
 	best_feature = features[best_feature_ind]
 
-	data = dataset[:,best_feature_ind]
-
+	# data = col_sparse(dataset,[best_feature_ind])
+	data = dataset[:,best_feature]
 	dTree={best_feature:{}}
-	
-	data_uniques = np.unique(data)
 
-	for u in data_uniques:
-		subfeatures=features.copy()
-		row_inds = list(np.where(data==u))[0]
-		if(len(row_inds)==0):
-			dTree[best_feature][u]=majorLabel(labels)
+	if(best_feature<=7 or best_feature==18):
+
+	# data_uniques = np.unique(data[:,0])
+		data_uniques = np.unique(data)
+
+		for u in data_uniques:
+			subfeatures=features.copy()
+			row_inds = list(np.where(data==u))[0]
+			if(len(row_inds)==0):
+				dTree[best_feature][u]=majorLabel(labels)
+			else:
+				subdataset=row_sparse(dataset,row_inds)
+				subfeatures.pop(best_feature_ind)
+				# print(features)
+				dTree[best_feature][u]=TreeGenerate(subdataset,subfeatures, depth)
+	else:
+		# print(best_feature)
+		new_d = depth - 1
+		average,dneg_row_inds,dpos_row_inds = get_averageAndindex(data)
+		cond_s="< "+str(average)
+		cond_l=">= "+str(average)
+
+		if(len(dneg_row_inds)==0 or new_d==0):
+			return majorLabel(labels)
 		else:
-			subdataset=row_sparse(dataset,row_inds)
-			subfeatures.pop(best_feature_ind)
-			# print(features)
-			dTree[best_feature][u]=TreeGenerate(subdataset,subfeatures)
+			dneg_subset=row_sparse(dataset,dneg_row_inds)
+			dTree[best_feature][cond_s]=TreeGenerate(dneg_subset,features, new_d)
+
+		if(len(dpos_row_inds)==0 or new_d==0):
+			return majorLabel(labels)
+		else:
+			dpos_subset=row_sparse(dataset,dpos_row_inds)
+			dTree[best_feature][cond_l]=TreeGenerate(dpos_subset,features, new_d)
+		
+
 	return dTree
 
 		
@@ -213,4 +277,13 @@ def TreeGenerate(dataset, features):
 # features=[0,1,2,3]
 features = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
 
-TreeGenerate(data_set_np,features)
+# data_set_np = np.array([[0, 0.5, 'N'],
+#            [0, 0.9,'N'],
+#            [1, 1.2,'Y'],
+#            [2, 1,'Y'],
+#            [0, 0.85,'Y'],
+#            [0, 1.5,'N'],
+#            [0, 2,'Y']])
+# features=[0,1]
+depth = 6
+print(TreeGenerate(data_set_np,features, depth))
